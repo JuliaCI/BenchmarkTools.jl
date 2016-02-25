a7b72c340eb81ab75d2aa47cfe08f0a063b6e9f6
Jarrett Revels <jarrettrevels@gmail.com>
2016-02-25 13:53:05 -0500

    automatically flatten ntrials results

--- src/execution.jl
+++ src/execution.jl
@@ -28,5 +28,31 @@ end
 # ntrials #
 ###########
 
-ntrials(exe, trials::Number, seconds = NaN; kwargs...) = [execute(exe, seconds; kwargs...) for _ in 1:trials]
-ntrials(exe, trials; kwargs...) = [execute(exe, t; kwargs...) for t in trials]
+ntrials(exe, trials::Number, seconds = NaN; kwargs...) = flatten([execute(exe, seconds; kwargs...) for _ in 1:trials])
+ntrials(exe, trials; kwargs...) = flatten([execute(exe, t; kwargs...) for t in trials])
+
+flatten(items) = flatten(items, typeof(first(items)))
+flatten(trials, ::Type{Trial}) = trials
+
+function flatten(groups, ::Type{BenchmarkGroup})
+    ngroups = length(groups)
+    reference = first(groups)
+    group = BenchmarkGroup(reference.id, reference.tags)
+    for id in keys(reference.benchmarks)
+        trials = Vector{Trial}(ngroups)
+        for g in 1:ngroups
+            trials[g] = groups[g][id]
+        end
+        group[id] = trials
+    end
+    return group
+end
+
+function flatten(ensembles, ::Type{BenchmarkEnsemble})
+    nensembles = length(ensembles)
+    ensemble = BenchmarkEnsemble()
+    for id in keys(first(ensembles).groups)
+        ensemble[id] = restruct([e[id] for e in ensembles], BenchmarkGroup)
+    end
+    return ensemble
+end
