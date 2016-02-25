968f05d6370fe15a412ede75d88c4755bee4cbaa
Jarrett Revels <jarrettrevels@gmail.com>
2016-02-25 10:42:39 -0500

    add another example group

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,8 +2,14 @@ using BenchmarkTools
 
 ensemble = BenchmarkEnsemble()
 
-addgroup!(ensemble, "eig", ["linalg", "factorization"])
+addgroup!(ensemble, "eig", ["linalg", "factorization", "math"])
 
 for i in 1:10
-    ensemble["eig"][i] = @benchmarkable eig(rand($i, $i)) 0.1
+    ensemble["eig"][i] = @benchmarkable eig(rand($i, $i)) 1e-6
+end
+
+addgroup!(ensemble, "sin", ["trig", "math"])
+
+for i in 1:10
+    ensemble["sin"][i] = @benchmarkable sin($i) 1e-6
 end
