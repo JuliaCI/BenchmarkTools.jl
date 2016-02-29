02deb1342a7f61a60a4fa69585bafd46f794eab7
Jarrett Revels <jarrettrevels@gmail.com>
2016-02-29 14:47:21 -0500

    TrialMetrics -> TrialRatio

--- src/trials.jl
+++ src/trials.jl
@@ -17,21 +17,21 @@ allocs(t::Trial) = fld(t.allocs, t.evals)
 
 Base.isless(a::Trial, b::Trial) = isless(time(a), time(b))
 
-################
-# TrialMetrics #
-################
+##############
+# TrialRatio #
+##############
 
-immutable TrialMetrics
+immutable TrialRatio
     time::Float64
     gctime::Float64
     memory::Float64
     allocs::Float64
 end
 
-Base.time(t::TrialMetrics) = t.time
-gctime(t::TrialMetrics) = t.gctime
-memory(t::TrialMetrics) = t.memory
-allocs(t::TrialMetrics) = t.allocs
+Base.time(t::TrialRatio) = t.time
+gctime(t::TrialRatio) = t.gctime
+memory(t::TrialRatio) = t.memory
+allocs(t::TrialRatio) = t.allocs
 
 function ratio(a::Real, b::Real)
     if a == b # so that ratio(0.0, 0.0) returns 1.0
@@ -41,10 +41,10 @@ function ratio(a::Real, b::Real)
 end
 
 function ratio(a::Trial, b::Trial)
-    return TrialMetrics(ratio(time(a),   time(b)),
-                        ratio(gctime(a), gctime(b)),
-                        ratio(memory(a), memory(b)),
-                        ratio(allocs(a), allocs(b)))
+    return TrialRatio(ratio(time(a),   time(b)),
+                      ratio(gctime(a), gctime(b)),
+                      ratio(memory(a), memory(b)),
+                      ratio(allocs(a), allocs(b)))
 end
 
 ##################
@@ -54,7 +54,7 @@ end
 const DEFAULT_TOLERANCE = 0.2
 
 immutable TrialJudgement
-    ratio::TrialMetrics
+    ratio::TrialRatio
     time::Symbol
     memory::Symbol
     allocs::Symbol
@@ -65,7 +65,7 @@ memory(t::TrialJudgement) = t.memory
 allocs(t::TrialJudgement) = t.allocs
 ratio(t::TrialJudgement) = t.ratio
 
-function TrialJudgement(ratio::TrialMetrics, tolerance = DEFAULT_TOLERANCE)
+function TrialJudgement(ratio::TrialRatio, tolerance = DEFAULT_TOLERANCE)
     return TrialJudgement(ratio,
                           judge(time(ratio),   tolerance),
                           judge(memory(ratio), tolerance),
@@ -77,7 +77,7 @@ function TrialJudgement(a::Trial, b::Trial, tolerance = DEFAULT_TOLERANCE)
 end
 
 judge(a::Trial, b::Trial, tolerance = DEFAULT_TOLERANCE) = TrialJudgement(a, b, tolerance)
-judge(ratio::TrialMetrics, tolerance = DEFAULT_TOLERANCE) = TrialJudgement(ratio, tolerance)
+judge(ratio::TrialRatio, tolerance = DEFAULT_TOLERANCE) = TrialJudgement(ratio, tolerance)
 
 function judge(ratio::Real, tolerance = DEFAULT_TOLERANCE)
     if isnan(ratio) || (ratio - tolerance) > 1.0
@@ -142,8 +142,8 @@ function Base.show(io::IO, t::Trial)
     print(io,   "  allocs:  ", allocs(t))
 end
 
-function Base.show(io::IO, t::TrialMetrics)
-    println(io, "BenchmarkTools.TrialMetrics: ")
+function Base.show(io::IO, t::TrialRatio)
+    println(io, "BenchmarkTools.TrialRatio: ")
     println(io, "  time:   ", time(t))
     println(io, "  gctime: ", gctime(t))
     println(io, "  memory: ", memory(t))
@@ -159,5 +159,5 @@ function Base.show(io::IO, t::TrialJudgement)
 end
 
 Base.showcompact(io::IO, t::Trial) = print(io, "Trial(", prettytime(time(t)), ")")
-Base.showcompact(io::IO, t::TrialMetrics) = print(io, "TrialMetrics(", prettypercent(time(t)), ")")
+Base.showcompact(io::IO, t::TrialRatio) = print(io, "TrialRatio(", prettypercent(time(t)), ")")
 Base.showcompact(io::IO, t::TrialJudgement) = print(io, "TrialJudgement(", prettydiff(time(ratio(t))), " => ", time(t), ")")
