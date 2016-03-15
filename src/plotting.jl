3ef7cbf3ef34afcf682c62f39111e36f45d7674a
Jarrett Revels <jarrettrevels@gmail.com>
2016-03-15 16:48:27 -0400

    plot interpolation line between points of scatter plot

--- src/plotting.jl
+++ src/plotting.jl
@@ -4,6 +4,7 @@ using PyPlot
 function PyPlot.plot(t::BenchmarkTools.Trial)
     a, b = linreg(t.evals, t.times)
     plt[:scatter](t.evals, t.times)
+    plot(t.evals, t.times)
     return plot(t.evals, [a + b*i for i in t.evals])
 end
 
