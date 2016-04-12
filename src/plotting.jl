a9ffd61db2226427a30f6f7297d16c0429f49911
Jarrett Revels <jarrettrevels@gmail.com>
2016-04-12 16:10:17 -0400

    allow pltlintrial to accept times and evals from any source

--- src/plotting.jl
+++ src/plotting.jl
@@ -5,8 +5,7 @@ KernelDensity.kde(t::BenchmarkTools.Trial) = kde(t.times)
 PyPlot.plot(t::BenchmarkTools.Trial) = plt[:scatter](0:length(t)-1, t.times)
 PyPlot.plot(k::KernelDensity.UnivariateKDE) = PyPlot.plot(k.x, k.density)
 
-function pltlintrial(b, logscale = true; kwargs...)
-    times, evals = BenchmarkTools.lineartrial(b; kwargs...)
+function pltlintrial(times, evals, logscale = true)
     plt[:scatter](evals, times ./ evals)
     logscale && (plt[:yscale]("log"); plt[:xscale]("log"))
 end
