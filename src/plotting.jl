4f8e87e149d8a3a73609ed7f943788a84a229737
Jarrett Revels <jarrettrevels@gmail.com>
2016-04-13 14:27:07 -0400

    enable kwargs for KDEs of Trials

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,7 +1,7 @@
 using KernelDensity
 using PyPlot
 
-KernelDensity.kde(t::BenchmarkTools.Trial) = kde(t.times)
+KernelDensity.kde(t::BenchmarkTools.Trial; kwargs...) = kde(t.times; kwargs...)
 PyPlot.plot(t::BenchmarkTools.Trial) = plt[:scatter](0:length(t)-1, t.times)
 PyPlot.plot(k::KernelDensity.UnivariateKDE) = PyPlot.plot(k.x, k.density)
 
