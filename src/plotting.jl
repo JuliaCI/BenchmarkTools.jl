095b4359576a5bf7ea35270e9821902b455c385d
Jarrett Revels <jarrettrevels@gmail.com>
2016-05-26 09:44:34 -0400

    simplify plotting methods

--- src/plotting.jl
+++ src/plotting.jl
@@ -2,23 +2,8 @@ using KernelDensity
 using PyPlot
 
 KernelDensity.kde(t::BenchmarkTools.Trial; kwargs...) = kde(t.times; kwargs...)
-PyPlot.plot(t::BenchmarkTools.Trial) = plt[:scatter](0:length(t)-1, t.times)
-PyPlot.plot(k::KernelDensity.UnivariateKDE) = PyPlot.plot(k.x, k.density)
-
-# function plotlintrial(times, evals, logscale = true)
-#     plt[:scatter](evals, times ./ evals)
-#     logscale && (plt[:yscale]("log"); plt[:xscale]("log"))
-# end
-
-function plotkde(b::BenchmarkTools.Benchmark, rep = 1; cut = 0.1, bandwidth = nothing,  kwargs...)
-    trials = Vector{BenchmarkTools.Trial}()
-    for _ in 1:rep
-        t = run(b; kwargs...)
-        push!(trials, t)
-    end
-    plotkde(trials; cut = cut, bandwidth = bandwidth)
-    return trials
-end
+PyPlot.plot(t::BenchmarkTools.Trial; kwargs...) = scatter(0:length(t)-1, t.times; kwargs...)
+PyPlot.plot(k::KernelDensity.UnivariateKDE; kwargs...) = PyPlot.plot(k.x, k.density; kwargs...)
 
 function plotkde(trials::Vector; kwargs...)
     for t in trials
@@ -29,5 +14,5 @@ end
 function plotkde(trial::BenchmarkTools.Trial; cut = 0.1, bandwidth = nothing, kwargs...)
     trial = BenchmarkTools.trim(trial, cut)
     k = bandwidth == nothing ? kde(trial) : kde(trial; bandwidth = bandwidth)
-    plot(k)
+    plot(k; kwargs...)
 end
