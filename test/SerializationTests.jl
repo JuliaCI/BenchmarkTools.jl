299caada99152b190bdc31bbd0517b59d16f0c15
Alex Arslan <ararslan@comcast.net>
2017-10-19 17:11:12 -0700

    Migrate serialization format to JSON

--- test/SerializationTests.jl
+++ test/SerializationTests.jl
@@ -1,14 +1,85 @@
 module SerializationTests
 
-using Base.Test
+using Compat
+using Compat.Test
 using BenchmarkTools
 
-old_data = BenchmarkTools.load(joinpath(dirname(@__FILE__), "data_pre_v006.jld"), "results")
-BenchmarkTools.save(joinpath(dirname(@__FILE__), "tmp.jld"), "results", old_data)
-new_data = BenchmarkTools.load(joinpath(dirname(@__FILE__), "tmp.jld"), "results")
+eq(x::T, y::T) where {T<:Union{BenchmarkTools.SUPPORTED_TYPES...}} =
+    all(i->eq(getfield(x, i), getfield(y, i)), 1:fieldcount(T))
+eq(x::T, y::T) where {T} = isapprox(x, y)
 
-@test old_data == new_data
+function withtempdir(f::Function)
+    d = mktempdir()
+    try
+        cd(f, d)
+    finally
+        rm(d, force=true, recursive=true)
+    end
+    nothing
+end
 
-rm(joinpath(dirname(@__FILE__), "tmp.jld"))
+@testset "Successful (de)serialization" begin
+    b = @benchmarkable sin(1)
+    tune!(b)
+    bb = run(b)
+
+    withtempdir() do
+        tmp = joinpath(pwd(), "tmp.json")
+
+        BenchmarkTools.save(tmp, b, bb)
+        @test isfile(tmp)
+
+        results = BenchmarkTools.load(tmp)
+        @test results isa Vector{Any}
+        @test length(results) == 2
+        @test eq(results[1], b)
+        @test eq(results[2], bb)
+    end
+end
+
+@testset "Deprecated behaviors" begin
+    b = @benchmarkable sin(1)
+    tune!(b)
+    bb = run(b)
+
+    @test_throws ArgumentError BenchmarkTools.save("x.jld", b)
+    @test_throws ArgumentError BenchmarkTools.save("x.txt", b)
+    @test_throws ArgumentError BenchmarkTools.save("x.json")
+    @test_throws ArgumentError BenchmarkTools.save("x.json", 1)
+
+    withtempdir() do
+        tmp = joinpath(pwd(), "tmp.json")
+        @test_warn "Naming variables" BenchmarkTools.save(tmp, "b", b)
+        @test isfile(tmp)
+        results = BenchmarkTools.load(tmp)
+        @test length(results) == 1
+        @test eq(results[1], b)
+    end
+
+    @test_throws ArgumentError BenchmarkTools.load("x.jld")
+    @test_throws ArgumentError BenchmarkTools.load("x.txt")
+    @test_throws ArgumentError BenchmarkTools.load("x.json", "b")
+end
+
+@testset "Error checking" begin
+    withtempdir() do
+        tmp = joinpath(pwd(), "tmp.json")
+        open(tmp, "w") do f
+            print(f, """
+            {"never":1,"gonna":[{"give":3,"you":4,"up":5}]}
+            """)
+        end
+        try
+            BenchmarkTools.load(tmp)
+            error("madness")
+        catch err
+            # This function thows a bunch of ArgumentErrors, so test for this specifically
+            @test err isa ArgumentError
+            @test contains(err.msg, "Unexpected JSON format")
+        end
+    end
+
+    @test_throws ArgumentError BenchmarkTools.recover([1])
+end
 
 end # module
