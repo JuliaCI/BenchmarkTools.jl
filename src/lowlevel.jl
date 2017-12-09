0802ed926f284611ccafdf1e0f41525f2f8710d3
Valentin Churavy <v.churavy@gmail.com>
2017-12-09 16:51:05 -0500

    make escape a bit more robust

--- src/lowlevel.jl
+++ src/lowlevel.jl
@@ -28,11 +28,7 @@ function _llvmname(typ::Type)
                     (Any, Ptr{Bool}), typ, isboxed_ref)
     name = unsafe_string(
         ccall(:LLVMPrintTypeToString, Cstring, (Ptr{Void},), llvmtyp))
-    if isboxed_ref[]
-        return name * "*"
-    else
-        return name
-    end
+    return (isboxed_ref[], name)
 end
 
 """
@@ -44,14 +40,25 @@ intended to add little to no overhead.
 See: https://youtu.be/nXaxk27zwlk?t=2441
 """
 @generated function escape(val::T) where T
-    # We need to get the darn LLVMName to be able to issue a
-    # fake call. There is probably a better way to do this.
-    name = _llvmname(T)
-    ir = """
-        call void asm sideeffect "", "X,~{memory}"($name %0)
-        ret void
-    """
-    quote 
-        llvmcall($ir, Void, Tuple{T}, val)
+    # If the value is `nothing` then a memory clobber
+    # should have the same effect.
+    if T == Void
+        return :(clobber())
+    end
+    # We need to get the string representation of the LLVM type to be able to issue a
+    # fake call.
+    isboxed, name = _llvmname(T)
+    if isboxed
+        # name will be `jl_value_t*` which we can't use since string based llvmcall can't handle named structs...
+        # Ideally we would issue a `bitcast jl_value_t* %0 to i8*`
+        Base.warn_once("Trying to escape a boxed value. Don't know how to handle that.")
+    else
+        ir = """
+            call void asm sideeffect "", "X,~{memory}"($name %0)
+            ret void
+        """
+        quote
+            llvmcall($ir, Void, Tuple{T}, val)
+        end
     end
 end
