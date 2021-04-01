4c229e2f38c189f9efddc9c6ab48f26ef98aabb0
Roger-Luo <rogerluo.rl18@gmail.com>
2021-03-31 21:03:04 -0400

    fix error

--- src/groups.jl
+++ src/groups.jl
@@ -310,7 +310,7 @@ macro benchmarkset(title, ex)
 end
 
 macro case(title, xs...)
-    esc(:(Symbol("#suite#")[$title] = @benchmarkable $(xs...)))
+    esc(:($(Symbol("#suite#"))[$title] = @benchmarkable $(xs...)))
 end
 
 function benchmarkset_m(title, ex::Expr)
@@ -343,9 +343,9 @@ end
 function benchmarkset_block(title, ex::Expr)
     stack = GlobalRef(BenchmarkTools, :benchmark_stack)
     quote
-        let Symbol("#root#") = last($stack)
-            Symbol("#root#")[$title] = Symbol("#root#") = BenchmarkGroup()
-            push!($stack, Symbol("#root#"))
+        let $(Symbol("#root#")) = last($stack)
+            $(Symbol("#root#"))[$title] = $(Symbol("#suite#")) = BenchmarkGroup()
+            push!($stack, $(Symbol("#suite#")))
             $ex
             pop!($stack)
         end
