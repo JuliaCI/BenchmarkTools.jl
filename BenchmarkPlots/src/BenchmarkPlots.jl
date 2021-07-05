51a918d1e85eff46a793717b1f91150da5098200
gustaphe <david.e.gustavsson@gmail.com>
2021-07-05 16:53:18 +0200

    Add plot capability (#194)
    * Add plots recipes for single Trial or BenchmarkGroup of Trials
    
    Co-authored-by: Valentin Churavy <vchuravy@users.noreply.github.com>

---
+++ BenchmarkPlots/src/BenchmarkPlots.jl
@@ -0,0 +1,25 @@
+module BenchmarkPlots
+using RecipesBase
+using BenchmarkTools: Trial, BenchmarkGroup
+
+@recipe function f(::Type{Trial}, t::Trial)
+    seriestype --> :violin
+    legend --> false
+    yguide --> "t / ns"
+    xticks --> false
+    t.times
+end
+
+@recipe function f(g::BenchmarkGroup, keys=keys(g))
+    legend --> false
+    yguide --> "t / ns"
+    for k in keys
+        @series begin
+            label --> string(k)
+            xticks --> true
+            [string(k)], g[k]
+        end
+    end
+end
+
+end
