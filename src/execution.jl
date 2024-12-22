f6f75c06e8c8ef490b795c218fa8630a26d6f627
singularitti <singularitti@outlook.com>
2024-12-21 20:58:35 -0700

    Add docs for `@ballocations`

--- src/execution.jl
+++ src/execution.jl
@@ -641,6 +641,20 @@ macro ballocated(args...)
     )
 end
 
+"""
+    @ballocations expression [other parameters...]
+
+Similar to the `@allocations` macro included with Julia,
+this macro evaluates an expression, discarding the resulting
+value, and returns the total number of allocations made
+during its execution.
+
+Unlike `@allocations`, it uses the `@benchmark` macro from
+the `BenchmarkTools` package, and accepts all of the same
+additional parameters as `@benchmark`. The returned number
+of allocations corresponds to the trial with the *minimum*
+elapsed time measured during the benchmark.
+"""
 macro ballocations(args...)
     return esc(
         quote
