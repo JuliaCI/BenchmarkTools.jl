33082ebf302eeaac7863f1f7e585fd3ab41499b1
singularitti <singularitti@outlook.com>
2024-12-21 20:54:16 -0700

    Add docstring for `@btimed`

--- src/execution.jl
+++ src/execution.jl
@@ -684,6 +684,22 @@ macro btime(args...)
     )
 end
 
+"""
+    @btimed expression [other parameters...]
+
+Similar to the `@timed` macro included with Julia, this
+macro executes an expression and returns a `NamedTuple`
+containing the value of the expression, the minimum elapsed
+time in seconds, the total bytes allocated, the number of
+allocations, and the garbage collection time in seconds
+during the benchmark.
+
+Unlike `@timed`, it uses the `@benchmark` macro from the
+`BenchmarkTools` package for more detailed and consistent
+performance measurements. The elapsed time reported is the
+minimum time measured during the benchmark. It accepts all
+additional parameters supported by `@benchmark`.
+"""
 macro btimed(args...)
     _, params = prunekwargs(args...)
     bench, trial, result = gensym(), gensym(), gensym()
