3e554d04cf93ac7c46cb650213ef8165a7bdea19
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-07-17 07:58:27 -0400

    Instead change formatting test to use a separate action

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,14 +7,6 @@ print("Testing code quality...")
 took_seconds = @elapsed Aqua.test_all(BenchmarkTools)
 println("done (took ", took_seconds, " seconds)")
 
-if VERSION >= v"1.6"
-    print("Testing code formatting...")
-    took_seconds = @elapsed @test JuliaFormatter.format(
-        BenchmarkTools; verbose=false, overwrite=false
-    )
-    println("done (took ", took_seconds, " seconds)")
-end
-
 print("Testing Parameters...")
 took_seconds = @elapsed include("ParametersTests.jl")
 println("done (took ", took_seconds, " seconds)")
