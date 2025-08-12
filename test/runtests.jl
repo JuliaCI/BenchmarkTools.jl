4102e74400b69fe1f3ac187fcb3f060a7dd322a6
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-08-12 09:24:40 -0400

    Use standard formatting .yml action (#397)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,10 @@
 using Aqua
 using BenchmarkTools
-using JuliaFormatter
 using Test
 
 print("Testing code quality...")
 took_seconds = @elapsed Aqua.test_all(BenchmarkTools)
 println("done (took ", took_seconds, " seconds)")
-
-if VERSION >= v"1.6"
-    print("Testing code formatting...")
-    took_seconds = @elapsed @test JuliaFormatter.format(
-        BenchmarkTools; verbose=false, overwrite=false
-    )
-    println("done (took ", took_seconds, " seconds)")
-end
 
 print("Testing Parameters...")
 took_seconds = @elapsed include("ParametersTests.jl")
