655d663eb073d038395fc5a72421a26eab830039
David Anthoff <anthoff@berkeley.edu>
2026-08-04 10:50:12 -0700

    Use packagedef file structure

--- src/BenchmarkTools.jl
+++ src/BenchmarkTools.jl
@@ -1,93 +1,9 @@
 module BenchmarkTools
 
 using JSON
-using Base.Iterators
-
-using Logging: @logmsg, LogLevel
-using Statistics
-using UUIDs: uuid4
-using Printf
-using Profile
 using Compat
-
 using PrecompileTools: @compile_workload, @setup_workload
 
-##############
-# Parameters #
-##############
-
-include("parameters.jl")
-
-export loadparams!
-
-##############
-# Trial Data #
-##############
-
-include("trials.jl")
-
-export gctime,
-    memory,
-    allocs,
-    params,
-    ratio,
-    judge,
-    isinvariant,
-    isregression,
-    isimprovement,
-    median,
-    mean,
-    std,
-    var,
-    rmskew!,
-    rmskew,
-    trim
-
-##################
-# Benchmark Data #
-##################
-
-include("groups.jl")
-
-export BenchmarkGroup,
-    invariants,
-    regressions,
-    improvements,
-    @tagged,
-    addgroup!,
-    @benchmarkset,
-    @case,
-    clear_empty!
-
-######################
-# Execution Strategy #
-######################
-
-include("execution.jl")
-
-export tune!,
-    warmup,
-    @ballocated,
-    @ballocations,
-    @benchmark,
-    @benchmarkable,
-    @belapsed,
-    @btime,
-    @btimed,
-    @bprofile
-
-#################
-# Serialization #
-#################
-
-include("serialization.jl")
-
-@setup_workload begin
-    @compile_workload begin
-        s = @benchmark 1 + 1
-        io = IOContext(IOBuffer(), :color => true)
-        show(io, MIME("text/plain"), s)
-    end
-end
+include("packagedef.jl")
 
 end # module BenchmarkTools
